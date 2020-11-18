9abe2009c11331cb66544f31d43ea00345d0c5e8
Kristinn <kthk8@hi.is>
2020-11-18 15:23:35 +0000

    Navið er nú skalanlegt :)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import HomeButton from '../svg/Home-Btn.svg'
-import ProjectsButton from '../svg/Projects-Btn.svg'
-import SkilsButton from '../svg/Skils-Btn.svg'
-import ContactButton from '../svg/Contact-Btn.svg'
-import AboutButton from '../svg/About-Btn.svg'
-
+//-----Icons-----//
+import HomeButton from '../svg/Home-Btn.svg';
+import ProjectsButton from '../svg/Projects-Btn.svg';
+import SkilsButton from '../svg/Skils-Btn.svg';
+import ContactButton from '../svg/Contact-Btn.svg';
+import AboutButton from '../svg/About-Btn.svg';
+//-----Styles-----//
+import '../styles/Nav.css';
 
 function Nav() {
     return (
-        <div className="Nav-wrapper">
-            <nav className="Nav">
+        <nav className="Nav">
+            <div className="Nav-Grid">
+                <Link className="Nav-Projects Nav-Link" to="/projects">
+                    <img className="Nav-Link-img" src={ProjectsButton} alt="Verkefnin" />
+                </Link>
+                <Link className="Nav-Skills Nav-Link" to="/skils">
+                    <img className="Nav-Link-img" src={SkilsButton} alt="Hvað ég kann" />
+                </Link>
                 <Link className="Nav-HomeBtn Nav-Link" to="/">
                     <img src={HomeButton} alt="Heim" />
                 </Link>
-                <Link className="Nav-Projects Nav-Link" to="/projects">
-                    <img src={ProjectsButton} alt="Verkefnin" />
-                </Link>
-                <Link className="Nav-Skills Nav-Link" to="/skils">
-                    <img src={SkilsButton} alt="Hvað ég kann" />
-                </Link>
                 <Link className="Nav-Contact Nav-Link" to="/contact">
-                    <img src={ContactButton} alt="Hafa samband" />
+                    <img className="Nav-Link-img" src={ContactButton} alt="Hafa samband" />
                 </Link>
                 <Link className="Nav-About Nav-Link" to="/about">
-                    <img src={AboutButton} alt="Um mig" />
+                    <img className="Nav-Link-img" src={AboutButton} alt="Um mig" />
                 </Link>
-            </nav>
-        </div>
+            </div>
+            <div className="Nav-Effect"></div>
+        </nav>
     );
 }
 
