38aa289da424da9f9922c8777cc35fb3a0c49b62
Kristinn <kthk8@hi.is>
2020-12-06 12:00:32 +0000

    Skipti um "icons" í NAV yfir í Font.Awesome.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,34 +2,64 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 //-----Icons-----//
 import HomeButton from '../svg/Home-Btn.svg';
-import ProjectsButton from '../svg/Projects-Btn.svg';
-import SkilsButton from '../svg/Skils-Btn.svg';
-import ContactButton from '../svg/Contact-Btn.svg';
-import AboutButton from '../svg/About-Btn.svg';
 //-----Styles-----//
 import '../styles/Nav.scss';
+//-----Font Awesome Imports-----//
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard, faAt, faChartBar, faTasks } from '@fortawesome/free-solid-svg-icons';
 
-function Nav({lightOn}) {
+function Nav({ lightOn }) {
     return (
         <nav className="Nav">
             <div className="Nav-Grid">
-                <NavLink className="Nav-Projects Nav-Link" to="/projects" activeClassName="selected">
-                    <img className="Nav-Link-img" src={ProjectsButton} alt="Verkefnin" />
+                <NavLink
+                    className="Nav-Projects Nav-Link"
+                    to="/projects"
+                    activeClassName="selected" >
+                    <FontAwesomeIcon
+                        icon={faTasks}
+                        className="Nav-Link-icon"
+                        alt="Verkefnin"
+                        style={{ color: `${lightOn ? '#293AD9' : '#29D9B9'}` }} />
                 </NavLink>
-                <NavLink className="Nav-Skills Nav-Link" to="/skils" activeClassName="selected">
-                    <img className="Nav-Link-img" src={SkilsButton} alt="Hvað ég kann" />
+                <NavLink
+                    className="Nav-Skills Nav-Link"
+                    to="/skils"
+                    activeClassName="selected">
+                    <FontAwesomeIcon
+                        icon={faChartBar}
+                        className="Nav-Link-icon"
+                        alt="Hvað ég kann"
+                        style={{ color: `${lightOn ? '#293AD9' : '#29D9B9'}` }} />
                 </NavLink>
-                <NavLink className="Nav-HomeBtn Nav-Link" exact to="/" activeClassName="selected">
+                <NavLink
+                    className="Nav-HomeBtn Nav-Link"
+                    exact to="/"
+                    activeClassName="selected">
                     <img src={HomeButton} alt="Heim" />
                 </NavLink>
-                <NavLink className="Nav-Contact Nav-Link" to="/contact" activeClassName="selected">
-                    <img className="Nav-Link-img" src={ContactButton} alt="Hafa samband" />
+                <NavLink
+                    className="Nav-Contact Nav-Link"
+                    to="/contact"
+                    activeClassName="selected">
+                    <FontAwesomeIcon
+                        icon={faAt}
+                        className="Nav-Link-icon"
+                        alt="Hafa samband"
+                        style={{ color: `${lightOn ? '#293AD9' : '#29D9B9'}` }} />
                 </NavLink>
-                <NavLink className="Nav-About Nav-Link" to="/about" activeClassName="selected">
-                    <img className="Nav-Link-img" src={AboutButton} alt="Um mig" />
+                <NavLink
+                    className="Nav-About Nav-Link"
+                    to="/about"
+                    activeClassName="selected">
+                    <FontAwesomeIcon
+                        icon={faAddressCard}
+                        className="Nav-Link-icon"
+                        alt="Um mig"
+                        style={{ color: `${lightOn ? '#293AD9' : '#29D9B9'}` }} />
                 </NavLink>
             </div>
-            <div className={`Nav-Effect${lightOn ? '--Light-Mode':''}`}></div>
+            <div className={`Nav-Effect${lightOn ? '--Light-Mode' : ''}`}></div>
         </nav>
     );
 }
